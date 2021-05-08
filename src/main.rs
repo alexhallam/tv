45b5ad2b2cf26e57a57956132f97cc4947c1ca38
Alex Hallam <alex.hallam@cfacorp.com>
2021-05-07 23:37:56 -0400

    use only a two column csv

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,87 @@
 use regex::Regex;
 use owo_colors::OwoColorize;
+use structopt::StructOpt;
+extern crate csv;
+use std::io;
+use itertools::Itertools;
 
+/// Search for a pattern in a file and display the lines that contain it.
+#[derive(StructOpt)]
+#[structopt(name = "tv", about = "Tidy Viewer (tv) uses column styling maximize viewing joy.")]
+struct Cli {
+//    #[structopt(parse(from_os_str),short = "i", long = "input")]
+//    input_csv_file_path: std::path::PathBuf,
+//    #[structopt(short = "c",long = "col_types")]
+//    column_types_override: String,
+}
 
 fn main() {
+ //   let args = Cli::from_args();
+    let rdr = csv::Reader::from_reader(io::stdin())
+        .records()
+        .into_iter()
+        .map(|x| x.expect("a csv record"))
+        .collect::<Vec<_>>();
+
+    // string to datatype inference
+    fn is_logical(text: &str) -> bool{
+    // col_logical -l, T,F,TRUE,FALSE,True,False,true,false,t,f,1,0
+        let rgex = r"^true$|^false$|^t$|^f$|TRUE$|^FALSE$|^T$|^F$|^True|^False";
+        let r = Regex::new(rgex).unwrap();
+        let lgl = r.is_match(&text);
+        return lgl
+    }
+    fn is_integer(text: &str) -> bool{
+    //let integer = "5";
+        let rgex = r"^([+-]?[1-9][0-9]*|0)$";
+        let r = Regex::new(rgex).unwrap();
+        let lgl = r.is_match(&text);
+        return lgl
+    }
+    fn is_double(text: &str) -> bool{
+    //let double = "0.01";
+        //https://www.geeksforgeeks.org/check-given-string-valid-number-integer-floating-point-java-set-2-regular-expression-approach/
+        let rgex = r"[+-]?\d+(\.\d+)?([Ee][+-]?\d+)?";
+        let r = Regex::new(rgex).unwrap();
+        let lgl = r.is_match(&text);
+        return lgl
+    }
+    fn is_time(text: &str) -> bool{
+    //let time = "11:59:37 UTC";
+        //https://stackoverflow.com/a/25873711
+        let rgex = r"^(?:[01]\d|2[0123]):(?:[012345]\d):(?:[012345]\d)$";
+        let r = Regex::new(rgex).unwrap();
+        let lgl = r.is_match(&text);
+        return lgl
+    }
+    //fn is_date(text: &str) -> bool{
+    //let date = "2020-01-01";
+    //    //https://mkyong.com/regular-expressions/how-to-validate-date-with-regular-expression/
+    //    return false 
+    //}
+    fn is_date_time(text: &str) -> bool{
+    //let datetime = "2020-10-09 11:59:37 UTC";
+        //https://stackoverflow.com/a/25873711
+        let rgex = r"^(?:[01]\d|2[0123]):(?:[012345]\d):(?:[012345]\d)";
+        let r = Regex::new(rgex).unwrap();
+        let lgl = r.is_match(&text);
+        return lgl
+    }
+    fn infer_type_from_string(text: &str) -> &str{
+        if is_time(text) == true {
+        return "time"
+        }else if is_integer(text) == true {
+        return "int"
+        }else if is_date_time(text) == true {
+        return "DT"
+        }else if is_double(text) == true {
+        return "double"
+        }else if is_logical(text) == true {
+        return "logical"
+        }else {
+        return "character"
+        }
+    }
 
     fn trunc_strings(vec_col: Vec<&str>, width: usize) -> Vec<String>{
         let ellipsis = '\u{2026}'.to_string();
@@ -44,10 +123,10 @@ fn main() {
         let width: usize= if float_has_point(text) == true {text.split(".").collect::<Vec<&str>>()[0].len()} else{text.len()};
         return width 
     }
-    fn char_pad(text: &String, max_width: usize) -> String{
-        let f = format!("{:>width$}", text, width = max_width).to_string();
-        return f
-    }
+    //fn char_pad(text: &String, max_width: usize) -> String{
+    //    let f = format!("{:>width$}", text, width = max_width).to_string();
+    //    return f
+    //}
     fn float_pad(text: &String, max_width: usize) -> String{
         let width = get_decimal_len(&text);
         let whole_number_width = get_left_decimal_len(&text);
@@ -63,51 +142,93 @@ fn main() {
         let string: String = if is_na == true {format_if_na(text)} else {float_pad(text, max_width)};
         return string;
     }
+    //let a = vec!["abc","abcde","abcdefgh","abcdefghijkl","","","abcdefghijklmnop"];
+    //let b = vec!["0.0001","0.001","0.01","0.1","1","","100"];
 
-    let a = vec!["abc","abcde","abcdefgh","abcdefghijkl","","","abcdefghijklmnop"];
-    let b = vec!["0.0001","0.001","0.01","0.1","1","","100"];
 
-    //split by decimal and count the largest decimal part 
-    let vec_len = b.clone()
+    let cols:usize = rdr[0].len();
+    let rows:usize = rdr.len();
+
+
+    // function to return the data type of a single column
+    //fn get_col_data_type(col: &csv::StringRecord){
+    fn get_col_data_type(col: Vec<&str>) -> &str{
+        // counts the frequency of the datatypes in the column 
+        // returns the most frequent. todo-make na not count and handle ties
+        let s = col
+        .into_iter()
+        .map(|x| infer_type_from_string(x))
+        .group_by(|&x| x)
+        .into_iter()
+        .map(|(key, group)| (key, group.count()))
+        .max_by_key(|&(_, count)| count)
+        .unwrap()
+        .0;
+        return s;
+    }
+
+    // csv gets records in rows. This makes them cols
+    let mut v:Vec<Vec<&str>> = vec![vec!["#"; rows as usize]; cols as usize];
+    for col in 0..cols{
+        for row in 0..rows{
+            v[col][row] = &rdr[row].get(col).unwrap();
+        }
+    }
+
+    // make datatypes vector
+   let mut vec_datatypes: Vec<&str> = vec!["#"; cols as usize];
+   for i in 0..cols{
+    vec_datatypes[i] = get_col_data_type(v[i].clone());
+    }
+   println!("{:?}", vec_datatypes);
+
+    // prep doubles
+    let vec_len = v[1]
+        .clone()
         .into_iter()
         .map(String::from)
         .map(|string| get_decimal_len(&string))
         .collect::<Vec<usize>>();
-    // to do learn how to convert this &usize to usize
-    let max_len = vec_len.iter().max().unwrap();
 
-    let chr = trunc_strings(a.clone(), 6);
-    let dbl = b.clone()
+    let max_len:&usize = vec_len.iter().max().unwrap();
+    let dbl = v[1].clone()
         .into_iter()
         .map(String::from)
-        .map(|string| float_format(&string, 4))
+        .map(|string| float_format(&string, *max_len))
         .collect::<Vec<String>>();
 
-    println!("--debug----debug----debug----debug----debug----debug----debug----debug----debug----debug----debug--");
-    println!("original chars: {:?}",a.clone());
-    println!("original doubles: {:?}",b);
+    // prep characters
+    let chr = trunc_strings(v[0].clone(), 6);//max_len
 
-    println!("original chars: {:?}",chr);
-    println!("original doubles: {:?}",dbl);
-    println!("--debug----debug----debug----debug----debug----debug----debug----debug----debug----debug----debug--");
+    //println!("--debug----debug----debug----debug----debug----debug----debug----debug----debug----debug----debug--");
+    //println!("original chars: {:?}",a.clone());
+    //println!("original doubles: {:?}",b);
+    //println!("original chars: {:?}",chr);
+    //println!("original doubles: {:?}",dbl);
+    //println!("--debug----debug----debug----debug----debug----debug----debug----debug----debug----debug----debug--");
 
-
-    println!("\t{} {}", "<pillar>".truecolor(129,161,193).bold().dimmed(), "<pillar>".truecolor(129,161,193).bold().dimmed());
-    println!("\t{}\t {}", "<char>".truecolor(129,161,193).bold().dimmed(), "<dbl>".truecolor(129,161,193).bold().dimmed());
-    for i in 0..a.len(){
-        if is_na(&chr[i]){
-        println!("\t{}\t {}",chr[i].truecolor(180,142,173).blink(),dbl[i].truecolor(240,248,255));
-        }else if is_na(&dbl[i]){
+    let meta_text = "tv dim:";
+    let div = "x";
+    println!("\t{} {} {} {}",meta_text.truecolor(143,188,187).dimmed(),
+                            rows.truecolor(143,188,187).dimmed(), 
+                            div.truecolor(143,188,187).dimmed(), 
+                            cols.truecolor(143,188,187).dimmed());
+    println!("\t{} {}", "<pillar>".truecolor(129,161,193), "<pillar>".truecolor(129,161,193));
+    println!("\t{}\t {}", "<char>".truecolor(143,188,187).dimmed(), "<dbl>".truecolor(143,188,187).dimmed());
+    for i in 0..rows{
+        if is_na(&chr[i])&is_na(&dbl[i]){
+        println!("\t{}\t {}",chr[i].truecolor(180,142,173),dbl[i].truecolor(180,142,173));
+        }else if is_na(&chr[i])&!is_na(&dbl[i]){
+        println!("\t{}\t {}",chr[i].truecolor(180,142,173),dbl[i].truecolor(240,248,255));
+        }else if is_na(&dbl[i])&!is_na(&chr[i]){
         println!("\t{}\t {}",chr[i].truecolor(240,248,255),dbl[i].truecolor(180,142,173));
         }else{
         println!("\t{}\t {}",chr[i].truecolor(240,248,255),dbl[i].truecolor(240,248,255));
         }
     }
 
-
  //let vs = tv::StringType(dbl);
- //  let vf = tv::StringType(chr);
-
- //  println!("{:^} {:^}", vs, vf);
+ //let vf = tv::StringType(chr);
+ //println!("{:^} {:^}", vs, vf);
 
 }
